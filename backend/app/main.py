e2f4100d2bec16a808740ef66fb4e329b36a2717
Rajatt09 <rajatbhati9559@gmail.com>
2025-01-19 22:35:56 +0530

    fix: deleted app folder from backend

--- backend/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.websocket import websocket_endpoint
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-
-# Import and include WebSocket routes
-app.include_router(websocket_endpoint)
